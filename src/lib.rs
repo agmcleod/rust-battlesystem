c30fd83b5a1a69748f13a48e556e7921be0e0303
Aaron McLeod <aaron.mcleod@mosaic.com>
2015-01-13 08:46:28 -0500

    working towards a command system

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+use std::collections::hash_map::HashMap;
+
 pub struct BattleSystem {
-  pub targets: Vec<int>
+  pub command_map: HashMap<String, usize>,
+  pub targets: Vec<isize>
+}
+
+impl BattleSystem {
+  pub fn exec_command(&self, command: &str) {
+    self.print_targets();
+  }
+  pub fn print_targets(&self) {
+    println!("Targets: {:?}", self.targets);
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,18 @@ extern crate battlesystem;
 fn main() {
   let battle_system = battlesystem::BattleSystem { targets: vec![1, 2] };
   let player_one_id = 1;
+
+  println!("Enter command");
+
+  loop {
+    let raw = std::io::stdin().read_line().ok().expect("Failed to read line");
+    let input = raw.trim();
+
+    if input == "quit" {
+      break;
+    }
+    else {
+      battle_system.exec_command(input);
+    }
+  }
 }
